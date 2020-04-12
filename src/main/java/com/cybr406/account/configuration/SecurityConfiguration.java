9f5fb771b3bd239bd64e32ebcb04e6d6a0d047de
Jacob Nutter <jacobnutter1@gmail.com>
2020-04-11 22:15:15 -0500

    account hw almost done

--- src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
+++ src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
@@ -1,22 +1,56 @@
 package com.cybr406.account.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+    @Bean
+    public User.UserBuilder userBuilder() {
+        User.UserBuilder builder = User.builder();
+        return builder;
+    }
+
+    @Bean
+    public User.UserBuilder userBuilder(PasswordEncoder passwordEncoder) {
+        User.UserBuilder builder = User.builder();
+        builder.passwordEncoder(passwordEncoder::encode);
+        return builder;
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,7 +63,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/check-user").hasAnyRole("ADMIN", "SERVICE")
+                .mvcMatchers(HttpMethod.GET, "/", "/**").permitAll()
+                .mvcMatchers(HttpMethod.POST, "/signup").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
--- src/main/java/com/cybr406/account/security/user/UserController.java
+++ src/main/java/com/cybr406/account/security/user/UserController.java
@@ -1,9 +1,62 @@
 package com.cybr406.account.security.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.*;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
+
+    private static final GrantedAuthority userRole = new SimpleGrantedAuthority("ROLE_USER");
+
+    @Autowired
+    private UserDetailsManager userDetailsManager;
+
+    @Autowired(required = true)
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/check-user")
+    private ResponseEntity<List<String>> checkUser(
+        @RequestHeader(value = "x-username", required = true) String username,
+        @RequestHeader(value = "x-password", required = true) String password) throws Exception {
+
+        try {
+            UserDetails userDetails = userDetailsManager.loadUserByUsername(username);
+
+            if (Objects.isNull(userDetails))
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+            if (!passwordEncoder.matches(password, userDetails.getPassword()))
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+            if (userDetails.getAuthorities().stream().anyMatch(ga -> !Objects.equals(ga, userRole))) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            return new ResponseEntity<>(userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList()),
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    }
 
     /*
      * The job of the UserController is to receive a username and a password, and if they are correct, return the
@@ -18,4 +71,4 @@ public class UserController {
      * new to you. If you prefer, simple for loops can accomplish the same thing with a little more typing.
      */
 
-}
+
